6585245f2d95f728afb7196739984909d6e7b929
Syed Shahmeer Mustafa Gillani <sg23bg@fsu.edu>
2025-10-15 05:40:04 -0400

    Fix BlogServiceProxy: remove duplicate code, fix braces, move Delete into class

--- lib.wordpress/BlogServiceProxy.cs
+++ lib.wordpress/BlogServiceProxy.cs
@@ -1,7 +1,68 @@
 ﻿using System;
-namespace lib.wordpress;
+using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using lib.wordpress.Models;
 
-public class BlogServiceProxy
+namespace lib.wordpress
 {
+    public class BlogServiceProxy
+    {
+        private List<Blog?> blogposts;
+        private static BlogServiceProxy? instance;
 
+        private BlogServiceProxy()
+        {
+            blogposts = new List<Blog?>();
+        }
+
+        public static BlogServiceProxy Current
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new BlogServiceProxy();
+                }
+
+                return instance;
+            }
+        }
+
+        public List<Blog?> Blogs => blogposts;
+
+        public Blog? AddOrUpdate(Blog? blog)
+        {
+            if (blog == null) return null;
+            if (blog.Id <= 0)
+            {
+                var maxId = -1;
+                if (blogposts.Any())
+                {
+                    maxId = blogposts.Select(b => b?.Id ?? -1).Max();
+                }
+                else
+                {
+                    maxId = 0;
+                }
+                blog.Id = ++maxId;
+                blogposts.Add(blog);
+            }
+
+            return blog;
+        }
+
+        public Blog? Delete(int id)
+        {
+            var blogToDelete = blogposts
+                .Where(b => b != null)
+                .FirstOrDefault(b => (b?.Id ?? -1) == id);
+
+            if (blogToDelete != null)
+                blogposts.Remove(blogToDelete);
+
+            return blogToDelete;
+        }
+    }
 }
